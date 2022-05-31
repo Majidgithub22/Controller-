f2a7f5a61ecbafdf70ced931d925a845910c458c
Majidgithub22 <101578863+Majidgithub22@users.noreply.github.com>
2022-05-31 11:46:21 +0500

    Add new oroject

--- My project (1)/Assets/Scripts/Scriptable/Session.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "Session", menuName = "ScriptableObject/Session")]
-public class Session : ScriptableObject {
-    public int level;
-    public int carNumber;
-    public int totalLevel;
-    public int unlocklevels;
-}
